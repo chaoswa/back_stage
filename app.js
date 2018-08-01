6eadbe16afa92bf5daaddd0d3445809fd34dfe0b
chaoswa <294982548@qq.com>
2018-08-01 15:57:43 +0800

    add product

--- app.js
+++ app.js
@@ -15,15 +15,46 @@ app.use(bodyParser.json())
 
 
 //连接数据库，数据库操作，安装mongodb
+// 1.启动mongodb服务，mongod --dbpath F:\mongodb;
+// 2.连接mongodb数据库，mongo
 var MongoClient=require('mongodb').MongoClient
 const DbUrl="mongodb://localhost:27017"
 const dbName = 'productmanage';
-// 1.启动mongodb服务，mongod --dbpath F:\mongodb;
-// 2.连接mongodb数据库，mongo
+
+//session中间件保存用户信息
+var session = require('express-session')
+app.use(session({
+    secret:'this is string key',
+    resave:false,
+    saveUninitialized:true,
+    cookie:{maxAge:1000*60*30},
+    rolling:true
+}))
+
+
+//自定义中间件，判断登录状态
+app.use(function(req,res,next){
+    console.log(req.url)
+    if(req.url=='/login' || req.url=='/doLogin'){
+        next()
+    }else{
+        if(req.session.userInfo && req.session.userInfo.username!=''){
+
+            //ejs中设置全局数据，所以页面都可以使用
+            app.locals['userInfo']=req.session.userInfo
+
+            next()
+        }else{
+            res.redirect('/login')
+        }
+    }
+})
 
 
 
 
+
+/******************* 配置路由**************************/
 app.get('/',function(req,res){
     res.send('index')
 })
@@ -45,11 +76,16 @@ app.post('/doLogin',function(req,res){
         const db=client.db(dbName)
 
         var result=db.collection('user').find(req.body)
+
         //遍历数据
         result.toArray(function(err,data){
             console.log(data)
             if(data.length>0){
                 console.log('登录成功')
+
+                //保存用户信息
+                req.session.userInfo=data[0]
+
                 res.redirect('/product')
             }else{
                 console.log('登录失败 ')
@@ -63,7 +99,30 @@ app.post('/doLogin',function(req,res){
 
 //商品
 app.get('/product',function(req,res){
-    res.render('product')
+
+    //连接数据库，查询数据
+    MongoClient.connect(DbUrl,{useNewUrlParser:true},function(err,client){
+        if(err){
+            console.log(err);
+            return;
+        }
+        const db=client.db(dbName)
+        var result=db.collection('product').find()
+        result.toArray(function(error,data){
+            if(error){
+                console.log(err);
+                return;
+            }
+            console.log(data)//得到查询的数据
+
+            //将数据渲染到模板
+            res.render('product',{
+                list:data
+            })
+        })
+    })
+
+    
 })
 
 //增加商品
@@ -79,6 +138,19 @@ app.get('/productEdit',function(req,res){
 //删除商品
 app.get('/productDelete',function(req,res){
     res.send('productDelete')
+})
+
+//退出登录
+app.get('/loginOut',function(req,res){
+    //销毁session
+    req.session.destroy(function(err){
+        if(err){
+            console.log(err)
+        }else{
+            res.redirect('/login')
+        }
+        
+    })
 })
 
 app.listen(3000,'127.0.0.1')
